ba0a645b18422f034fe9a520f70330b41704c497
PetrutzaNagy <petrutza_nagy@yahoo.com>
2022-08-16 20:31:34 +0300

    push tests

---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+import selenium.webdriver
+import pytest
+import selenium.webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+@pytest.fixture
+def browser():
+    #before tests
+    global driver
+    # initializam un browser
+    s = Service(ChromeDriverManager().install())
+    driver = selenium.webdriver.Chrome(service=s)
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+    #return driver
+    yield driver
+    #after tests
+    driver.quit()
+    return driver
---
+++ tests/test_add_remove_elements_page.py
@@ -0,0 +1,37 @@
+import time
+from selenium.webdriver.common.by import By
+
+from pages.add_remove_elements_page import AddRemoveElementsPage
+
+
+def test_check_add_element_functionality(browser):
+    add_remove_page = AddRemoveElementsPage(browser)
+    # deschidem o pagina
+    add_remove_page.load_page()
+    add_remove_page.clickAddButton()
+    assert add_remove_page.isAddButtonDisplayed()
+
+
+
+def test_check_url():
+    pass
+
+def test_title():
+    pass
+
+def test_link():
+    pass
+
+def test_add_and_delete_functionality(browser):
+    # deschidem o pagina
+    browser.get('https://the-internet.herokuapp.com/add_remove_elements/')
+    add_element_button = browser.find_element(By.CSS_SELECTOR, "[onclick='addElement()']")
+    for i in range(10):
+      add_element_button.click()
+    delete_button_list = browser.find_elements(By.CLASS_NAME, "added-manually")
+    assert len(delete_button_list) == 10, "Not all delete button is displayed"
+    for i in range(10):
+        delete_button_list[0].click()
+        delete_button_list = browser.find_elements(By.CLASS_NAME, "added-manually")
+        assert len(delete_button_list) == 10-i-1, "Not all delete button is displayed"
+
